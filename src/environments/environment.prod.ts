aece7840037c8e458748f67f7537850b553823b9
horaciocoronel <arielc@gmail.com>
2018-04-02 10:16:02 -0400

    Add firebase enironments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+	firebase: {
+		apiKey: "AIzaSyCkAsI7uKKre2NiXtrIlZxp8xZ_Xz3r7Ng",
+		authDomain: "clientpanelprod-826f4.firebaseapp.com",
+		databaseURL: "https://clientpanelprod-826f4.firebaseio.com",
+		projectId: "clientpanelprod-826f4",
+		storageBucket: "clientpanelprod-826f4.appspot.com",
+		messagingSenderId: "292042988245"
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+	firebase: {
+		apiKey: "AIzaSyCkAsI7uKKre2NiXtrIlZxp8xZ_Xz3r7Ng",
+    authDomain: "clientpanelprod-826f4.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-826f4.firebaseio.com",
+    projectId: "clientpanelprod-826f4",
+    storageBucket: "clientpanelprod-826f4.appspot.com",
+    messagingSenderId: "292042988245"
+	}
 };

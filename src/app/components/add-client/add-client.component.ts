e9420277c071ced5d44af55492862b347930da29
horaciocoronel <arielc@gmail.com>
2018-04-02 11:51:18 -0400

    Add flash messages to add-client.component

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+// Import Client model
+import { Client } from '../../models/Client';
 
 @Component({
   selector: 'app-add-client',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-client.component.css']
 })
 export class AddClientComponent implements OnInit {
+	// Define an empty Client object
+	client: Client = {
+		firstName: '',
+		lastName: '',
+		email: '',
+		phone: '',
+		balance: 0
+	}
+	// We can't add a balance when we add a new client
+	disableBalanceOnAdd: boolean = true;
 
-  constructor() { }
+	@ViewChild('clientForm') form :any;
+
+  constructor(private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
   }
+	onSubmit({value, valid}: {value: Client, valid: boolean}) {
+		if(this.disableBalanceOnAdd) {
+			value.balance = 0;
+		}
+		if(!valid) {
+			// Show error
+			this.flashMessage.show('Please fill out the form correctly', { cssClass: 'alert-danger', timeout: 3000
+		});
+		} else {
+		// Add new Client
+		// Show message
+		this.flashMessage.show('New Client Added', { cssClass: 'alert-success', timeout: 3000
+	});
+		// Redirect to Dashboard
+	}
+	}
 
 }

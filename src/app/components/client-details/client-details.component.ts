a768cb7f4878482b79962a82e3b5a785c9530f1c
horaciocoronel <arielc@gmail.com>
2018-04-03 11:49:32 -0400

    Add update balance button and its functionality

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -35,5 +35,10 @@ export class ClientDetailsComponent implements OnInit {
 		this.client = client;
 	})
   }
+	updateBalance() {
+		this.clientService.updateClient(this.client);
+		this.flashMessage.show('Balance updated', { cssClass: 'alert-success', timeout: 3500
+		});
+	}
 
 }

5875e69d95db0caa0f2ec1332171e4beb0966800
horaciocoronel <arielc@gmail.com>
2018-04-03 15:58:21 -0400

    Link settings service with settings component and add local storage to save settings

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -5,13 +5,20 @@ import { Settings } from '../models/Settings';
 export class SettingsService {
 	settings: Settings = {
 		allowRegistration: false,
-		disableBalanceOnAdd: false,
+		disableBalanceOnAdd: true,
 		disableBalanceOnEdit: true
 	}
 
-  constructor() { }
+  constructor() {
+		if(localStorage.getItem('settings') != null) {
+			this.settings = JSON.parse(localStorage.getItem('settings'));
+		}
+	 }
 
-	getSettings() : Settings {
-		return this.settings
+	getSettings(): Settings {
+		return this.settings;
+	}
+	changeSettings(settings: Settings) {
+		localStorage.setItem('settings', JSON.stringify(settings));
 	}
 }
